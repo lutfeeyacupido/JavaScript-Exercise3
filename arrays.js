6b30ae3bbd558bdfec9b2baf2cbd212f49b7aaa2
lutfeeyacupido <cupidlikeperson@gmail.com>
2025-10-31 09:35:08 +0200

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,13 @@
+let colors = []; 
+colors.push("red"); 
+colors.push("green");
+colors.push("black");
+
+let numbers = [1, 2, 3, 4, 5];
+
+colors.pop("red");
+
+colors.unshift("purple");
+
+console.log("The length of the colors array is:", colors.length);
+console.log("The length of the numbers array is:", numbers.length);
